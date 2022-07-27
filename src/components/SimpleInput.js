f969c6214362546cd1d8650f05f6415ff2c57604
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-27 20:46:38 +0200

    submissions and input values

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,37 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  // v. 1 (useRef)
+  // const nameInputRef = useRef();
+  // v. 2 (useState)
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+
+    console.log(enteredName);
+    // v. 1
+    // const enteredValue = nameInputRef.current.value;
+
+    // v. 1 reseting the value, manipulating the DOM
+    // nameInputRef.current.value = '';
+
+    // v. 2 reseting the value
+    setEnteredName('');
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        {/* v. 1 */}
+        {/* <input ref={nameInputRef} */}
+
+        <input type="text" id="name" onChange={nameInputChangeHandler} value={enteredName} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
